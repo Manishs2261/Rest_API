55c01cadee73e368770e777be8f899eb01082004
Manish sahu <sahusanju138@gmail.com>
2023-08-08 16:30:13 +0530

    create a post model and add api

--- lib/src/screen/home/home.dart
+++ lib/src/screen/home/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../../model/PostModel.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,11 +13,82 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<PostModel> postList = [];
+  
+  Future<List<PostModel>> getPostApi () async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200)
+      {
+
+        postList.clear();
+        for(Map i in data)
+        {
+          postList.add(PostModel.fromJson(i));
+        }
+        return postList;
+
+      }else
+        {
+
+          return postList;
+        }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("home"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          children: [
+
+            Expanded(
+              child: FutureBuilder(
+                future: getPostApi(),
+                  builder: (context , snapshot){
+
+                  if(!snapshot.hasData){
+                    return Text('Loading');
+                  }else
+                    {
+                      return ListView.builder(
+                        itemCount: postList.length,
+                          itemBuilder: (context,index){
+
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Title',style:TextStyle(
+                                    fontSize: 20,fontWeight: FontWeight.bold
+                                  )),
+                                  Text(postList[index].title.toString()),
+                                  SizedBox(height: 10,),
+                                  Text('Discription',style:TextStyle(
+                                      fontSize: 20,fontWeight: FontWeight.bold
+                                  )),
+                                  Text(postList[index].body.toString()),
+
+                                ],
+                              ),
+                            ),
+                          );
+
+                          });
+                    }
+
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
